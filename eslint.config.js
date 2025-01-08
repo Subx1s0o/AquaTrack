44a358a8d87ff54dc98e95bf9020ac987c4288a4
Subx1s0o <nazarsabardin@gmail.com>
2025-01-08 14:54:47 +0200

    fix: fix lint

--- eslint.config.js
+++ eslint.config.js
@@ -1,5 +1,5 @@
-
-import globals from 'globals'; 
+import js from '@eslint/js';
+import globals from 'globals';
 import reactHooks from 'eslint-plugin-react-hooks';
 import reactRefresh from 'eslint-plugin-react-refresh';
 import tseslint from '@typescript-eslint/eslint-plugin';
@@ -33,11 +33,21 @@ export default [
       'react-hooks': reactHooks,
       'react-refresh': reactRefresh,
       tailwindcss,
-      react,
-      ...prettier.rules
+      react
+    },
+    settings: {
+      react: {
+        version: 'detect'
+      }
     },
     rules: {
+      ...js.configs.recommended.rules,
+      ...tseslint.configs.recommended.rules,
+      ...react.configs.recommended.rules,
+      ...tailwindcss.configs.recommended.rules,
       ...reactHooks.configs.recommended.rules,
+      ...prettier.rules,
+      
       'react-refresh/only-export-components': [
         'warn',
         { allowConstantExport: false }
