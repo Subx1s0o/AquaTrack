f96b40a83a91623ec7f483264dc48dbcb6ca2152
Subx1s0o <nazarsabardin@gmail.com>
2025-01-08 15:46:31 +0200

    refactor:  simplify Home component

--- src/main.tsx
+++ src/main.tsx
@@ -1,25 +1,25 @@
-import { StrictMode } from "react";
-import { createRoot } from "react-dom/client";
-import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import { RouterProvider, createBrowserRouter } from 'react-router-dom';
 
-import "./index.css";
+import './index.css';
 
 const router = createBrowserRouter([
   {
-    path: "/",
-    element: <div className="flex w-dvw bg-gray-400 text-red-500">Home</div>,
+    path: '/',
+    element: <div>Home</div>,
   },
   {
-    path: "/about",
+    path: '/about',
     element: <div>About</div>,
   },
   {
-    path: "*",
+    path: '*',
     element: <div>404</div>,
   },
 ]);
 
-createRoot(document.getElementById("root")!).render(
+createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <RouterProvider router={router} />
   </StrictMode>,
