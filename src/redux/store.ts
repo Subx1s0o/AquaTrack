c5d0134657b9f96ea783f91819180e7785a83a2a
Subx1s0o <nazarsabardin@gmail.com>
2025-01-12 03:11:24 +0200

    feat: initialize redux store with water reducer setup

---
+++ src/redux/store.ts
@@ -0,0 +1,12 @@
+// import { configureStore } from '@reduxjs/toolkit';
+
+// import { waterReducer } from './water/slice';
+
+// export const store = configureStore({
+//   reducer: {
+//     water: waterReducer,
+//   },
+// });
+
+// export type RootState = ReturnType<typeof store.getState>;
+// export type AppDispatch = typeof store.dispatch;
