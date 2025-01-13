00bfe4d8d74db6c79561cbdd7acd6e3bdf7a645b
Anna Velychko <162054651+Annvel126V@users.noreply.github.com>
2025-01-13 23:22:02 +0100

    add HomeWelcomeContent

--- src/components/WelcomeContent/WelcomeContent.tsx
+++ src/components/WelcomeContent/WelcomeContent.tsx
@@ -1,9 +1,12 @@
 import { PropsWithChildren } from 'react';
 
+import HomeWelcomeContent from '../HomeWelcomeContent/HomeWelcomeContent';
+
 export default function WelcomeContent({ children }: PropsWithChildren) {
   return (
     <div className="absolute left-4 top-1/2 -translate-y-1/2 bg-grey md:left-[64px]">
       {children}
+      <HomeWelcomeContent />
     </div>
   );
 }
