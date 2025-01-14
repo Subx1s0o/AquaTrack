15efeb48c2ac07feb7a539407b97491ac8073310
Subx1s0o <nazarsabardin@gmail.com>
2025-01-14 03:55:24 +0200

    feat: home width fixes

--- src/pages/HomePage/HomePage.tsx
+++ src/pages/HomePage/HomePage.tsx
@@ -5,7 +5,7 @@ import Logo from '@/components/ui/Logo';
 
 export default function HomePage({ children }: PropsWithChildren) {
   return (
-    <div className="grid h-full grid-rows-1 gap-4 lg:h-auto lg:grid-cols-2">
+    <div className="grid size-full grid-rows-1 gap-4 lg:h-auto lg:grid-cols-2">
       <div className="relative rounded-[30px] bg-grey">
         <Logo />
 
