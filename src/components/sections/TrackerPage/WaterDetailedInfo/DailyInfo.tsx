d37ec560f5b773363d3f7a0210a5fc7d3b8161e4
Mariya Buzhava <bujava.masha@gmail.com>
2025-01-17 19:12:45 +0200

    add scroll

--- src/components/sections/TrackerPage/WaterDetailedInfo/DailyInfo.tsx
+++ src/components/sections/TrackerPage/WaterDetailedInfo/DailyInfo.tsx
@@ -6,7 +6,7 @@ export default function DailyInfo() {
   return (
     <div className="box-content flex h-40 w-[343px] flex-col bg-gray-200 p-5 md:h-[174px] md:w-[640px] md:px-8 lg:w-[608px]">
       <div className="flex items-center justify-between pb-6">
-        <ChooseDate date="2025-01-16" />
+        <ChooseDate date="2025-01-17" />
         <AddWaterBtn />
       </div>
 
--- src/components/sections/TrackerPage/WaterDetailedInfo/WaterList.tsx
+++ src/components/sections/TrackerPage/WaterDetailedInfo/WaterList.tsx
@@ -1,19 +1,67 @@
-import React from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 
 import { WaterItem } from './WaterItem';
 
 export const WaterList: React.FC = () => {
   const mockEntries = [
-    { id: 1, volume: 250, time: '8:00' },
-    { id: 2, volume: 300, time: '10:00' },
-    { id: 3, volume: 200, time: '12:00' },
+    { id: 1, volume: 250, time: '7:00' },
+    { id: 2, volume: 250, time: '11:00' },
+    { id: 3, volume: 250, time: '14:00' },
   ];
 
+  const scrollContainerRef = useRef<HTMLDivElement | null>(null);
+  const [isScrollable, setIsScrollable] = useState(false);
+  const [scrollProgress, setScrollProgress] = useState(0);
+
+  useEffect(() => {
+    const container = scrollContainerRef.current;
+    if (container) {
+      setIsScrollable(container.scrollWidth > container.clientWidth);
+
+      const progress =
+        (container.scrollLeft /
+          (container.scrollWidth - container.clientWidth)) *
+        100;
+      setScrollProgress(progress);
+    }
+  }, [mockEntries]);
+
+  const handleScroll = () => {
+    const container = scrollContainerRef.current;
+    if (container) {
+      const progress =
+        (container.scrollLeft /
+          (container.scrollWidth - container.clientWidth)) *
+        100;
+      setScrollProgress(progress);
+    }
+  };
+
   return (
-    <div className="flex h-[74px] w-[303px] gap-2 overflow-x-auto whitespace-nowrap md:h-[86px] md:w-[640px] md:gap-8 lg:w-[608px] lg:gap-4">
-      {mockEntries.map(entry => (
-        <WaterItem key={entry.id} entry={entry} />
-      ))}
+    <div className="relative">
+      <div
+        ref={scrollContainerRef}
+        onScroll={handleScroll}
+        className="box-content flex h-[74px] w-[303px] gap-2 overflow-x-auto scroll-smooth whitespace-nowrap pb-[24px] md:h-[86px] md:w-[640px] md:gap-8 lg:w-[608px] lg:gap-4"
+        style={{
+          scrollbarWidth: 'none',
+          msOverflowStyle: 'none',
+        }}
+      >
+        {mockEntries.map(entry => (
+          <WaterItem key={entry.id} entry={entry} />
+        ))}
+      </div>
+      {isScrollable && (
+        <div className="absolute h-[8px] w-[303px] rounded-[9px] bg-white md:w-[640px] lg:w-[608px]">
+          <div
+            className="h-2 rounded-[9px] bg-darkGrey transition-all duration-300"
+            style={{
+              width: `${scrollProgress}%`,
+            }}
+          ></div>
+        </div>
+      )}
     </div>
   );
 };
