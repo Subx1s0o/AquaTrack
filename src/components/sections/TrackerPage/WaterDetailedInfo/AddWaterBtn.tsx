48a0239313c1e0068fb8059165d735b66133ea14
Mariya Buzhava <bujava.masha@gmail.com>
2025-01-18 18:02:10 +0200

    add scroll, hover, focus

--- src/components/sections/TrackerPage/WaterDetailedInfo/AddWaterBtn.tsx
+++ src/components/sections/TrackerPage/WaterDetailedInfo/AddWaterBtn.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 export const AddWaterBtn: React.FC = () => {
   return (
     <div className="flex items-center gap-[10px]">
-      <button className="flex size-[30px] items-center justify-center rounded-[30px] bg-green text-3xl font-light text-darkGrey transition-colors lg:hover:bg-darkGrey lg:hover:text-white lg:focus-visible:bg-darkGrey lg:focus-visible:text-white">
+      <button className="flex size-[30px] items-center justify-center rounded-[30px] bg-green text-3xl font-light text-darkGrey transition-colors lg:hover:bg-darkGrey lg:hover:text-white lg:focus:bg-darkGrey lg:focus:text-white">
         +
       </button>
       <span className="font-poppins text-md font-bold text-darkGrey">
@@ -12,33 +12,3 @@ export const AddWaterBtn: React.FC = () => {
     </div>
   );
 };
-
-// import React, { useState } from 'react';
-
-// // import WaterModal from './WaterModal';
-
-// interface AddWaterBtnProps {
-//   onAddWater: (volume: number) => void;
-// }
-
-// export const AddWaterBtn: React.FC<AddWaterBtnProps> = () => {
-//   const [isModalOpen, setModalOpen] = useState(false);
-
-//   const toggleModal = () => setModalOpen(!isModalOpen);
-//   return (
-//     <div className="flex items-center gap-2">
-//       <button
-//         onClick={toggleModal}
-//         className="flex w-7 items-center justify-center rounded-full bg-green text-xl font-bold text-darkGrey transition-colors lg:hover:bg-darkGrey lg:hover:text-white lg:focus-visible:bg-darkGrey lg:focus-visible:text-white"
-//       >
-//         +
-//       </button>
-//       <span className="font-poppins text-md font-bold text-darkGrey">
-//         Add water
-//       </span>
-//       {/* {isModalOpen && (
-//         <WaterModal onClose={toggleModal} onAddWater={onAddWater} />
-//       )} */}
-//     </div>
-//   );
-// };
