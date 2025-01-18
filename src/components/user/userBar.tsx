c9843a3eb5496e1740e91ba3b7cf9d00be032d4e
Subx1s0o <nazarsabardin@gmail.com>
2025-01-18 23:50:33 +0200

    fix: add photo placeholder for avatar

--- src/components/user/userBar.tsx
+++ src/components/user/userBar.tsx
@@ -1,27 +1,31 @@
-import React, { useState } from "react";
-import { useAppSelector } from "@/redux/hooks";
-import { selectUser } from "@/redux/auth/selectors";
-import UserBarPopover from "./userBarPopover";
-import Icon from "../ui/Icon";
+import React, { useState } from 'react';
+
+import { selectUser } from '@/redux/auth/selectors';
+import { useAppSelector } from '@/redux/hooks';
+
+import Icon from '../ui/Icon';
+import UserBarPopover from './userBarPopover';
 
 function UserBar() {
-  const user = useAppSelector(selectUser); 
+  const user = useAppSelector(selectUser);
 
   const [isPopoverOpen, setPopoverOpen] = useState(false);
   const [isRotated, setIsRotated] = useState(false);
 
   if (!user) {
     return (
-      <div className="p-4 bg-gray-100 rounded-lg shadow-md">
-        <h1 className="text-2xl font-bold text-gray-800">User data not available</h1>
+      <div className="rounded-lg bg-gray-100 p-4 shadow-md">
+        <h1 className="text-2xl font-bold text-gray-800">
+          User data not available
+        </h1>
       </div>
     );
   }
 
-  let displayName = user.name || "Guest";
+  let displayName = user.name || 'Guest';
 
-  if (user.name === "User") {
-    displayName = user.email.split("@")[0]; 
+  if (user.name === 'User') {
+    displayName = user.email.split('@')[0];
   }
 
   const togglePopover = () => setPopoverOpen(!isPopoverOpen);
@@ -34,20 +38,20 @@ function UserBar() {
           togglePopover();
           toggleRotation();
         }}
-        className="inline-flex items-center gap-2 px-3 py-2 bg-gray-800 text-white rounded-full font-poppins text-[14px] font-bold leading-[18px] tracking-[-0.14px] text-center"
+        className="inline-flex items-center gap-2 rounded-full bg-gray-800 px-3 py-2 text-center font-poppins text-[14px] font-bold leading-[18px] tracking-[-0.14px] text-white"
       >
         <span>{displayName}</span>
         <img
-          src={user.avatarURL || "/default-avatar.png"}
+          src={user.avatarURL || '/images/default-avatar.avif'}
           alt="avatar"
-          className="w-8 h-8 sm:w-11 sm:h-11 lg:w-11 lg:h-11 rounded-full"
+          className="size-8 rounded-full sm:size-11 lg:size-11"
         />
 
         <Icon
-          id="icon-chevron-down" 
-          w={12} 
-          h={12} 
-          className={`transition-transform duration-300 ${isRotated ? "rotate-180" : ""}`} 
+          id="icon-chevron-down"
+          w={12}
+          h={12}
+          className={`transition-transform duration-300 ${isRotated ? 'rotate-180' : ''}`}
         />
       </button>
 
--- src/components/user/userPanel.tsx
+++ src/components/user/userPanel.tsx
@@ -1,14 +1,16 @@
 import React from 'react';
-import { useAppSelector } from '@/redux/hooks';
-import UserBar from './userBar';
-import { selectUser } from '@/redux/auth/selectors';
 
-function UserPanel() {
+import { selectUser } from '@/redux/auth/selectors';
+import { useAppSelector } from '@/redux/hooks';
+
+import UserBar from './userBar';
+
+export default function UserPanel() {
   const user = useAppSelector(selectUser);
 
   if (!user) {
     return (
-      <div className="p-4 bg-gray-100 rounded-lg shadow-md">
+      <div className="rounded-lg bg-gray-100 p-4 shadow-md">
         <h1 className="text-2xl font-bold text-gray-800">No user data</h1>
       </div>
     );
@@ -16,20 +18,16 @@ function UserPanel() {
 
   let displayName = user.name;
 
-  if (user.name === "User") {
-    displayName = user.email.split('@')[0]; 
+  if (user.name === 'User') {
+    displayName = user.email.split('@')[0];
   }
 
   return (
-    <div className="flex flex-col items-center gap-[20px] md:flex-row md:justify-between lg:flex-row md:justify-between">
-      <h2 className="text-darkGrey text-2xl font-normal leading-8 tracking-tight 
-md:text-3xl md:leading-9 md:tracking-tighter 
-lg:leading-9 lg:tracking-tighter">
+    <div className="flex flex-col items-center gap-[20px] md:flex-row md:justify-between lg:flex-row">
+      <h2 className="text-2xl font-normal leading-8 tracking-tight text-darkGrey md:text-3xl md:leading-9 md:tracking-tighter lg:leading-9 lg:tracking-tighter">
         Hello<span className="font-bold text-gray-700">, {displayName}!</span>
       </h2>
       <UserBar />
     </div>
   );
 }
-
-export default UserPanel;
