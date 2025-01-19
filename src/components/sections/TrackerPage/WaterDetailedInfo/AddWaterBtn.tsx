1e438744a20ad467557088eecd3e9d4647cc682b
Subx1s0o <nazarsabardin@gmail.com>
2025-01-19 22:45:54 +0200

    feat: add water add modal to Daily section

--- src/components/sections/TrackerPage/WaterDetailedInfo/AddWaterBtn.tsx
+++ src/components/sections/TrackerPage/WaterDetailedInfo/AddWaterBtn.tsx
@@ -1,14 +1,36 @@
+import { useState } from 'react';
+
+import Modal from '@/components/modals/Modal/Modal';
+import WaterModal from '@/components/modals/WaterModal/WaterModal';
 import Icon from '@/components/ui/Icon';
 
 export default function AddWaterBtn() {
+  const [isModalOpen, setIsModalOpen] = useState(false);
+
+  const handleOpenModal: () => void = () => {
+    setIsModalOpen(true);
+  };
+  const handleCloseModal: () => void = () => {
+    setIsModalOpen(false);
+  };
   return (
-    <div className="flex items-center gap-[10px]">
-      <button className="flex size-[30px] items-center justify-center rounded-[30px] bg-green text-3xl text-darkGrey transition-colors lg:hover:bg-darkGrey lg:hover:text-white lg:focus-visible:bg-darkGrey lg:focus-visible:text-white">
-        <Icon className="text-inherit" id="icon-plus" w={20} h={20} />
-      </button>
-      <span className="font-poppins text-md font-bold text-darkGrey">
-        Add water
-      </span>
-    </div>
+    <>
+      <div className="flex items-center gap-[10px]">
+        <button
+          onClick={handleOpenModal}
+          className="flex size-[30px] items-center justify-center rounded-[30px] bg-green text-3xl text-darkGrey transition-colors lg:hover:bg-darkGrey lg:hover:text-white lg:focus-visible:bg-darkGrey lg:focus-visible:text-white"
+        >
+          <Icon className="text-inherit" id="icon-plus" w={20} h={20} />
+        </button>
+        <span className="font-poppins text-md font-bold text-darkGrey">
+          Add water
+        </span>
+      </div>
+      {isModalOpen && (
+        <Modal isOpen={isModalOpen} onClose={handleCloseModal}>
+          <WaterModal type="add" onClose={handleCloseModal} />
+        </Modal>
+      )}
+    </>
   );
 }
