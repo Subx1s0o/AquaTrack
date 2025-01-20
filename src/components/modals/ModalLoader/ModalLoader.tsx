9f11cbe7ecbac9848ec1167f16a408f55997fa80
Nykyforuk Vasyl <nikiforuk.vasya@gmail.com>
2025-01-20 22:04:50 +0200

    ModalLoader

---
+++ src/components/modals/ModalLoader/ModalLoader.tsx
@@ -0,0 +1,45 @@
+import { PropsWithChildren } from 'react';
+import { Modal as ResponsiveModal } from 'react-responsive-modal';
+import 'react-responsive-modal/styles.css';
+
+import Icon from '../../ui/Icon';
+import Loader from '../../ui/Loader/Loader';
+import css from './mainModalStyles.module.css';
+
+interface ModalLoaderProps extends PropsWithChildren {
+  isOpen: boolean;
+  onClose: () => void;
+  isLoading: boolean;
+  isShowCloseIcon?: boolean;
+}
+
+const ModalLoader = ({
+  isOpen,
+  onClose,
+  isLoading,
+  isShowCloseIcon = true,
+  children,
+}: ModalLoaderProps) => {
+  return (
+    <ResponsiveModal
+      open={isOpen}
+      onClose={onClose}
+      focusTrapped={false}
+      center
+      closeIcon={
+        isShowCloseIcon ? (
+          <Icon id="icon-x" w={24} h={24} className={css.closeModalIcon} />
+        ) : null
+      }
+      classNames={{
+        overlay: css.mainModalOverlay,
+        modal: css.mainModalContent,
+        closeButton: css.closeMainModalButton,
+      }}
+    >
+      {isLoading ? <Loader /> : children}
+    </ResponsiveModal>
+  );
+};
+
+export default ModalLoader;
