93740c771ab95d2618ceeb1f3e90bdd7947e0f53
Subx1s0o <nazarsabardin@gmail.com>
2025-01-20 06:14:47 +0200

    fix: fixing layout, remove loader

--- src/SharedTrackerLayout.tsx
+++ src/SharedTrackerLayout.tsx
@@ -6,7 +6,7 @@ export default function SharedTrackerLayout({
   children: ReactNode;
 }) {
   return (
-    <div className="mx-auto flex h-screen min-h-[1396px] max-w-[375px] items-center p-4 font-poppins md:max-w-screen-md md:p-8 lg:max-w-screen-lg">
+    <div className="mx-auto flex h-screen min-h-[1396px] max-w-[375px] items-center p-4 font-poppins md:max-w-screen-md md:p-8 lg:min-h-full lg:max-w-screen-lg">
       <div className="flex min-h-[736px] w-full flex-col gap-4 lg:h-auto lg:flex-row">
         {children}
       </div>
--- src/components/common/PrivateRoute.tsx
+++ src/components/common/PrivateRoute.tsx
@@ -1,8 +1,6 @@
 import { ReactNode, useEffect } from 'react';
 import { Navigate } from 'react-router-dom';
 
-import Loader from '@/components/ui/Loader/Loader';
-
 import { getUser } from '@/redux/auth/operations';
 import { selectIsAuthenticated, selectIsLoading } from '@/redux/auth/selectors';
 import { useAppDispatch, useAppSelector } from '@/redux/hooks';
@@ -25,14 +23,6 @@ export default function PrivateRoute({
       dispatch(getUser());
     }
   }, [dispatch, isAuthenticated, loading]);
-
-  if (loading) {
-    return (
-      <div className="flex size-full items-center justify-center">
-        <Loader />
-      </div>
-    );
-  }
 
   return isAuthenticated ? <>{children}</> : <Navigate to={redirectTo} />;
 }
