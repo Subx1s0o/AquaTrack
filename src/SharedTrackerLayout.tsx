554beed5062184a6d0edc5f05c794ec1a445a060
Subx1s0o <nazarsabardin@gmail.com>
2025-01-20 03:51:06 +0200

    fix: fixing main tracker layout

--- src/SharedTrackerLayout.tsx
+++ src/SharedTrackerLayout.tsx
@@ -7,7 +7,7 @@ export default function SharedTrackerLayout({
 }) {
   return (
     <div className="mx-auto flex h-screen min-h-[1396px] max-w-[375px] items-center p-4 font-poppins md:max-w-screen-md md:p-8 lg:max-w-screen-lg">
-      <div className="grid min-h-[736px] w-full gap-4 lg:h-auto lg:grid-cols-2">
+      <div className="flex min-h-[736px] w-full flex-col gap-4 lg:h-auto lg:flex-row">
         {children}
       </div>
     </div>
